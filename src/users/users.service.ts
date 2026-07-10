89de93e4e1a5c14179699884c0d5db0c1f114215
imbrunosantoos <brunoribeirosouzasantos@gmail.com>
2026-07-10 17:56:17 +0100

    feat: add user model and users module

---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: { name: string; email: string; password: string }): Promise<User> {
+    return this.prisma.user.create({ data });
+  }
+
+  findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  findById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+}
